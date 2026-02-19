a378cc9bb6aaf3098b43a8f4d697518fc87ee554
molomono <bcarmstr@xs4all.nl>
2026-02-19 15:11:25 +0100

    fixing ai autocorrect line

--- control_modules/pp_control.py
+++ control_modules/pp_control.py
@@ -117,7 +117,7 @@ class PPControl(BaseController):
         u_ff = (self.t_ref - fist_layer_compensation) * self.k_ss + fan_speed * self.k_fan  
 
         
-        actual_velocity = self.motion_report.get_status(self.printer.get_prev_stats(0))['live_extruder_velocity']
+        actual_velocity = self.motion_report.get_status(read_time)['live_extruder_velocity']
         logging.info("PP-Control Control Effort: PID_PWM: %s, FF_PWM: %s" % (u_fb_pid, u_ff))
         logging.info("PP-Control velocity: %s, z_position: %s" % (actual_velocity, z_position))
 
