db39597cbae4c87471e19f185eec26482395396f
molomono <bcarmstr@xs4all.nl>
2026-02-22 01:46:52 +0100

    fix

--- control_modules/__init__.py
+++ control_modules/__init__.py
@@ -3,3 +3,5 @@ from .pp_control import PPControl
 from .ss_ff_gain import SSFFControl
 from .lead_lag_control import LeadLagControl
 from .simple_ff_fb_control import SimpleFFFBControl 
+
+from .pp_calibrate import PPCalibrate   
