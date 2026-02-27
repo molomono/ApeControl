fe3c6328a36b7b476c8d85d87c63a3232c15ae47
molomono <bcarmstr@xs4all.nl>
2026-02-27 16:51:07 +0100

    removed deprecated code

---
+++ control_modules/__init__.py
@@ -0,0 +1,5 @@
+from .base_controller import BaseController
+from .mpc_control import ControlMPC, MpcCalibrate
+from .pid_control import PIDControl
+from .pp_calibrate import PPCalibrate
+from .pp_control import PPControl
