7bd5e1d700d95042f2628940a3ddd2c0d0d92be7
molomono <bcarmstr@xs4all.nl>
2026-02-28 16:06:24 +0100

    try this

--- control_modules/mpc_control.py
+++ control_modules/mpc_control.py
@@ -59,7 +59,7 @@ class ControlMPC(BaseController):
 
     #def post_init(self, load_clean=False, register=True):
     #    self.handle_ready()
-        self.max_power = self.heater.get_max_power() * self.const_heater_power
+        self.max_power = self._heater.get_max_power() * self.const_heater_power
         self.want_ambient_refresh = self.ambient_sensor is not None
         self.state_block_temp = (
             AMBIENT_TEMP if load_clean else self._heater_temp()
